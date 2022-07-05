d847750a352519b1e49683efb175c82dc713cec1
violangige <violangige@gmail.com>
2022-07-05 11:18:55 -0700

    13

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_times_table - prints multiplication table
+ * @n: the int to print
+ *
+ * Return: Always 0.
+ */
+void print_times_table(int n);
+{
+	for (n = 0; n <= 15; n++)
+	{
+		_putchar ('\n');
+		return (0);
+	}
+}
