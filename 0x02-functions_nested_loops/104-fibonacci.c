ac6d91097a46fee89693488cdba525edc670dead
violangige <violangige@gmail.com>
2022-07-07 08:10:34 -0700

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 /**
- *main - check code for ALX School students
- *
- * Return: 0
+ * main - prints out first 98
+ * fibonacci suit numbers
+ * Return: return 0
  */
 int main(void)
 {
-	unsigned long a = 1, b = 2, f = 0;
-	int i;
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
+	unsigned long n1_h1, n1_h2, n2_h1, n2_h2;
+	unsigned long h1, h2;
 
-	printf("%lu, %lu, ", a, b);
-	for (i = 0; i < 95; i++)
+	for (inc = 0; inc < 92; inc++)
 	{
-		f = a + b;
+		n3 = n1 + n2;
+		printf("%lu, ", n3);
+		n1 = n2;
+		n2 = n3;
+	}
+	n1_h1 = n1 / 10000000000;
+	n2_h1 = n2 / 10000000000;
+	n1_h2 = n1 % 10000000000;
+	n2_h2 = n2 % 10000000000;
 
-		if (i % 2 == 0)
-			a = f;
+	for (inc = 93; inc < 99; inc++)
+	{
+		h1 = n1_h1 + n2_h1;
+		h2 = n1_h2 + n2_h2;
 
-		else
-			b = f;
-		printf("%lu", f);
-		if (i != 94)
+		if ((n1_h2 + n2_h2) > 9999999999)
+		{
+			h1 += 1;
+			h2 %= 10000000000;
+		}
+		printf("%lu%lu", h1, h2);
+		if (inc != 98)
 			printf(", ");
+
+		n1_h1 = n2_h1;
+		n1_h2 = n2_h2;
+		n2_h1 = h1;
+		n2_h2 = h2;
 	}
 	printf("\n");
 	return (0);
 }
+
