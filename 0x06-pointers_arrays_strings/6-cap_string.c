e64cefcf8a1b4b6250dfb8fcbcf11f9df83311e6
violangige <violangige@gmail.com>
2022-07-14 13:51:21 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,7 @@ char *cap_string(char *str)
 {
 	int index = 0;
 
-	while (str[++index])
+	while (str[index])
 	{
 		while (!(str[index] >= 'a' && (str[index] <= 'z'))
 				index++;
@@ -27,9 +27,11 @@ char *cap_string(char *str)
 					str[index - 1] == '(' ||
 					str[index - 1] == ')' ||
 					str[index - 1] == '{' ||
-					str[index - 1] == '}')
+					str[index - 1] == '}' ||
+				  index == 0)
 				str[index] -= 32;
-				}
-				return (str);
 
+				index++;
 				}
+	return (str);
+}
