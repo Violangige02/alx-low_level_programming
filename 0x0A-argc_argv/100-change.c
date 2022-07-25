39e578d2205d13119adb63d62f764c230881b7fc
violangige <violangige@gmail.com>
2022-07-25 11:06:24 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints the minimum number of coins to make change
- * for an amount of arguments
+ * for an amount of money
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: returns 1 if there is an error; else returns 0
@@ -18,7 +18,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	cents = atoi(argv[1]);
-
 	while (cents > 0)
 	{
 		cents++;
